012fdeb027766815f6d4cfc4e007e8d90353dc41
Samim Kamleyar <ahmadsamimkamelyar@gmail.com>
2020-05-29 12:03:41 -0700

    practice

--- static/js/script.js
+++ static/js/script.js
@@ -21,3 +21,30 @@ function genarateCat() {
   image.src = "https://gifimage.net/wp-content/uploads/2018/04/kedi-gif-tumblr.gif"
   div.appendChild(image);
 }
+
+function rpsGame(yourChoice) {
+  console.log(yourChoice);
+  let humanChoice, botChoice;
+  // humanChoice = yourChoice.id;
+  // botChoice = numberToChoice(randToRpsInt());
+  // results = decideWinner(humanChoice, botChoice); //[0, 1] human lost | bot won
+  // message = finalMessage(results);// {'message': 'you won!' , 'color':, 'grem'} 
+  rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+//function that choce randamly a number (rock, paper and sisor)
+function randToRpsInt() {
+  return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+  return ['rock', 'paper', 'scissors'][number]
+}
+
+function decideWinner(yourChoice, computerChoice) {
+  var rpsDatabase = {
+    'rock': {
+      
+    }
+  }
+}
